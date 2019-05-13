12d542b3d3e2b2504995b9dbfab98560369a5b99
Mathurin BODY <mathurin.body@gmail.com>
2019-05-13 23:59:38 +0200

    i18n

--- src/settings/SettingsScreen.js
+++ src/settings/SettingsScreen.js
@@ -8,7 +8,8 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-import {Searchbar} from "react-native-paper";
+import {Button, Searchbar, Title} from "react-native-paper";
+import I18n from '../i18n/i18n'
 
 type Props = {};
 
@@ -20,9 +21,16 @@ export default class SettingsScreen extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Settings Screen</Text>
+        <Title>{I18n.t("settings.selectLanguage")}</Title>
+        <Button onPress={this.onSwitchTo('fr')}>Français</Button>
+        <Button onPress={this.onSwitchTo('en')}>English</Button>
       </View>
     );
+  }
+
+  onSwitchTo = (newLocale) => () => {
+    I18n.locale = newLocale;
+    this.forceUpdate();
   }
 }
 
