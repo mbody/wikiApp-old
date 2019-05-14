cee278d77861379d0d60fce2378e1e9545eb6219
Mathurin BODY <mathurin.body@gmail.com>
2019-05-14 21:03:27 +0200

    favoritesScreen

--- src/favorites/FavoritesScreen.js
+++ src/favorites/FavoritesScreen.js
@@ -7,30 +7,101 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-import {Searchbar} from "react-native-paper";
+import {FlatList, Image, Platform, StyleSheet, Text, View} from 'react-native';
+import {Card, IconButton, Searchbar, Title} from "react-native-paper";
+import {addFavoriteAction, removeFavoriteAction} from "../redux/favorites";
+import {searchAction, searchClearAction} from "../redux/wikipedia";
+import {connect} from "react-redux";
+import {Colors} from "../Theme";
 
 type Props = {};
 
-export default class FavoritesScreen extends Component<Props> {
+class FavoritesScreen extends Component<Props> {
 
   state = {
   };
 
   render() {
+    const {favoritePages} = this.props;
+
     return (
       <View style={styles.container}>
-        <Text>Favorites Screen</Text>
+        <Title>Vos Favoris</Title>
+        <View style={styles.searchResultsContainer}>
+          {favoritePages && (favoritePages.length === 0 ?
+              <Text>Vous n'avez pas de favoris :-( </Text>
+              :
+              <FlatList data={favoritePages}
+                        style={styles.list}
+                        renderItem={this.renderPageCard}
+                        keyExtractor={(item, index) => index.toString()}
+              >
+
+              </FlatList>)
+          }
+        </View>
       </View>
     );
+  }
+
+  renderPageCard = ({item, index}) => {
+    return <Card key={'card_' + index} style={styles.card}>
+      <Card.Title key={'cardTitle_' + index} title={item.title} subtitle={item.description}
+                  left={(props) =>
+                      <Image {...props} key={'cardThumb_' + index}
+                             source={{uri: item.thumbnail && item.thumbnail.source}}
+                             style={{height: 45, width: 45, backgroundColor: '#ddd'}}/>
+                  }
+                  right={props =>
+                      <IconButton icon={'favorite'} color={Colors.gray}
+                                  size={30}
+                                  onPress={() => this.toggleFavorite(item)}/>
+                  }
+      />
+    </Card>
+  };
+
+
+  toggleFavorite = (page) => {
+      this.props.removeFavoriteAction(page);
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
-    padding:10
+    padding:10,
+    paddingTop: 50
   },
+  searchResultsContainer: {
+    marginTop: 20,
+    alignSelf: 'stretch',
+    alignItems: 'center'
+  },
+  list:{
+    alignSelf: 'stretch'
+  },
+  card: {
+    margin: 5,
+    backgroundColor: '#f9f9f9',
+  }
 });
+
+
+// here we're mapping state to props
+const mapStateToProps = state => {
+  const {favorites} = state;
+  return {
+    favoritePages: favorites.pages
+  };
+};
+
+// here we're mapping actions to props
+const mapDispatchToProps = (dispatch) => {
+  return {
+    removeFavoriteAction: (page) => dispatch(removeFavoriteAction(page)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(FavoritesScreen);
